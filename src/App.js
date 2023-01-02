193c547e2ab83c6f24fcc9174689bc2a494243c2
bkarabo754 <bkmasango26@gmail.com>
2023-01-02 22:38:37 +0200

    updating the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
-import { CTA, Brand, Navbar } from './components'
-import './App.css'
+import React from 'react';
 
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
-const App = () => {
-  return (
-    <div classNAme="App">
-      <div className="gradient_gb">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
