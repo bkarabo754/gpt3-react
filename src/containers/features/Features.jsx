8a0a74dc42ecba0e80ee8ab248ece36640a8e29c
bkarabo754 <bkmasango26@gmail.com>
2023-01-03 21:23:35 +0200

    adding the GPT-3 section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,7 +3,12 @@ import './features.css'
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding' id="features">
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+    </div>
   )
 }
 
